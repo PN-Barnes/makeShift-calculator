810c80d3ab0b296a677591df58aa601233bc4903
Paul N Barnes <paulieb99@gmail.com>
2023-01-17 21:20:53 -0800

    working on seperating the button selection

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ let result = 0;
 
 buttons.addEventListener('click', (event) => {
   const button = event.target.innerHTML;
-  screen.innerHTML += button;
   console.log(button);
+  if (isNaN(button)) {
+    console.log(button);
+  } else {
+    screen.innerHTML += button;
+    console.log(button);
+  }
 });

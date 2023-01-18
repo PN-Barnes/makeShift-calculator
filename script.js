ff24ed61a5ed3956c12e2e6583894099ae3cf083
Paul N Barnes <paulieb99@gmail.com>
2023-01-17 21:31:40 -0800

    fixed the event listener to only respond when a button is clicked

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const screen = document.getElementById('calculatorScreen');
 screen.innerHTML = '';
 const buttons = document.getElementById('buttons');
+const allClear = document.getElementById('allClear');
 let result = 0;
 
 buttons.addEventListener('click', (event) => {
-  const button = event.target.innerHTML;
-  console.log(button);
-  if (isNaN(button)) {
+  if (event.target.tagName === 'BUTTON') {
+    const button = event.target.innerHTML;
     console.log(button);
+    if (isNaN(button)) {
+      console.log(button);
+    } else {
+      screen.innerHTML += button;
+      console.log(button);
+    }
   } else {
-    screen.innerHTML += button;
-    console.log(button);
+    return;
   }
+});
+
+allClear.addEventListener('click', (e) => {
+  const screen = e.target.innerHTML;
+  screen.innerHTML = '';
 });

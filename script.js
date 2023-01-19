ef590157a30afc93d0db986818a0fd49ee3fa379
Paul N Barnes <paulieb99@gmail.com>
2023-01-18 18:02:57 -0800

    set the operator variable to be set when a math operator is clicked, all clear also resets the screen and operator variable

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ let screen = document.getElementById('calculatorScreen');
 screen.innerHTML = '';
 const buttons = document.getElementById('buttons');
 const allClear = document.getElementById('allClear');
+let operator = null;
 
 let result = 0;
 
 allClear.addEventListener('click', (event) => {
   console.log(event.target.innerHTML);
   screen.innerHTML = '';
+  operator = null;
 });
 buttons.addEventListener('click', (event) => {
   if (event.target.tagName === 'BUTTON') {
     const button = event.target.innerHTML;
-    console.log(button);
     if (isNaN(button)) {
-      console.log(button);
+      operator = button;
+      console.log(operator);
     } else {
       screen.innerHTML += button;
       console.log(button);

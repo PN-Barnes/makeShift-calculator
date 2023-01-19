89431839633d25bef7bf90a5e634d5316d5c233f
Paul N Barnes <paulieb99@gmail.com>
2023-01-18 18:13:17 -0800

    created an array to save the first number before the operator is selected and prepare the screen for second number input

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ screen.innerHTML = '';
 const buttons = document.getElementById('buttons');
 const allClear = document.getElementById('allClear');
 let operator = null;
+let numbers = [];
 
 let result = 0;
 
@@ -12,11 +13,14 @@ allClear.addEventListener('click', (event) => {
   operator = null;
 });
 buttons.addEventListener('click', (event) => {
-  if (event.target.tagName === 'BUTTON') {
+  if (event.target.tagName === 'BUTTON' && event.target.innerHTML !== '=') {
     const button = event.target.innerHTML;
     if (isNaN(button) && button !== '=') {
+      numbers.push(screen.innerHTML);
       operator = button;
+      screen.innerHTML = '';
       console.log(operator);
+      console.log(numbers);
     } else {
       screen.innerHTML += button;
       console.log(button);
